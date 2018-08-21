25bffe781b7fe18e99763b5df852e8ca6c74ad07
linjoh88 <lin.joh@live.se>
2018-08-21 13:58:54 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,3 +5,24 @@
  * @package Superlist Child
  * @since Superlist Child 1.0.0
  */
+
+
+function wpb_add_google_fonts() {
+ 
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i|Playfair+Display:400,400i,700,700i,900,900i', false ); 
+}
+ 
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+ 
+function superlist_child_enqueue_files() {
+    # Remove original style
+    # wp_dequeue_style( 'superlist' );
+
+    # Register style for custom appearance
+    wp_register_style( 'style', get_stylesheet_directory_uri() . '/style.css' );
+
+    # Include new styles
+    wp_enqueue_style( 'style' );
+}
+
+add_action( 'wp_enqueue_scripts', 'superlist_child_enqueue_files' );

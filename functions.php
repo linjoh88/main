d87677529d595a14e6e0e1ec4547d5f8cbb9e09c
Linda Johansson <lin.joh@live.se>
2018-08-29 21:44:07 +0200

    Tagit bort funktioner
    Tagit bort funktioner i functions för att dropdown och filter ska kunna fungera.

--- functions.php
+++ functions.php
@@ -21,31 +21,10 @@ function superlist_child_enqueue_files() {
     # Register style for custom appearance
     wp_register_style( 'superlist-custom', get_stylesheet_directory_uri() . '/superlist-custom.css' );
 
-    # Include new styles
-	wp_enqueue_style( 'bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' );
-	wp_enqueue_style( 'superlist-custom' );
+
+   
 }
 
 add_action( 'wp_enqueue_scripts', 'superlist_child_enqueue_files', 99 );
 
-function theme_js() {
-
-	global $wp_scripts;
-
-	wp_enqueue_script( 'bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
-	# wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js');
-
-}
-
-add_action( 'wp_enqueue_scripts', 'theme_js');
-
-add_image_size( 'single-post-thumbnail', 1140, 656 );
-
-function change_the_header($url_for_image) {
-    if (is_home()) 
-        $url_for_image = 'http://localhost:81/v6/wp-content/uploads/2018/08/logo_grön.png';
-
-    return $url_for_image;
-}
-add_filter('theme_mod_header_image', 'change_the_header');
 
